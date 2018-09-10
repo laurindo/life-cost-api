5b99127dc682022bf2a35a0bfca14bfcef87ad1c
Daniel Laurindo <dslaurindo@gmail.com>
2018-09-10 16:17:35 -0300

    Adding graphql client and video to app

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,19 +1,25 @@
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import ExhangeRate from './ExchangeRates';
+
+const client = new ApolloClient({
+  uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql'
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <video autoPlay={true} loop="" muted={true} className="video-full" id="bgvideo">
+            <source src="https://ak03-video-cdn.slidely.com/promoVideos/videos/5b/96/5b96c11f7a97fd9706829fd9/preview.mp4?dv=3" type="video/mp4" />
+          </video>
+          <ExhangeRate />
+        </div>
+      </ApolloProvider>
     );
   }
 }
